1138210d501124ac2698d39aa36191946cca79e7
ily8340 <96059753+ily8340@users.noreply.github.com>
2022-02-24 00:12:16 +0200

    Update index.js
    feat: add morse

--- src/index.js
+++ src/index.js
@@ -38,9 +38,30 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let morseS = '',
+        morseR ='';
+    for (let a = 0; a < expr.length; a += 10){
+        morseS = expr.slice(a, a + 10);
+        if (morseS == "*".repeat(10)) {
+            morseR += ' ';
+            continue;
+        }
+        let result = '',
+            morseWord = '';
+        for(let h = 0; h<morseS.length; h += 2){
+            morseWord = morseS.slice(h, h + 2);
+            if(morseWord == '10') {
+                result += '.';
+            }
+            if(morseWord == "11") {
+                result += '-';
+            }
+        }
+        morseR += MORSE_TABLE[result];
+    }
+    return morseR;
 }
 
 module.exports = {
     decode
-}
+};
